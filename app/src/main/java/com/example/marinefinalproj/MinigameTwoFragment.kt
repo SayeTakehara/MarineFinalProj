ef01d133c2cb54ee861780e878568c3775d48f02
takeharas24 <sayetakehara2545@gmail.com>
2023-04-21 10:37:01 -0400

    minigame 2 began

--- app/src/main/java/com/example/marinefinalproj/MinigameTwoFragment.kt
+++ app/src/main/java/com/example/marinefinalproj/MinigameTwoFragment.kt
@@ -1,16 +1,19 @@
 package com.example.marinefinalproj
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.marinefinalproj.databinding.FragmentMinigameOneBinding
 import com.example.marinefinalproj.databinding.FragmentMinigameTwoBinding
+import android.view.GestureDetector
+import android.view.MotionEvent
 
 class MinigameTwoFragment : Fragment() {
     private var _binding : FragmentMinigameTwoBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mDetector: GestureDetectorCompat
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,6 +21,21 @@ class MinigameTwoFragment : Fragment() {
         _binding = FragmentMinigameTwoBinding.inflate(inflater, container, false)
         val rootView = binding.root
         //Minigame two: fling something? Maybe sand
+        mDetector = GestureDetectorCompat(this.requireContext(), MyGestureListener())
         return rootView
+    }
+
+
+    private class MyGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onFling(
+            event1: MotionEvent,
+            event2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            Log.d("MainActivity", "onFling: $event1 $event2")
+            return true
+        }
     }
 }
