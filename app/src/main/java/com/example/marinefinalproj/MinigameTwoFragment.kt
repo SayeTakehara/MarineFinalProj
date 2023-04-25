679c63f071ecd5d3f215bed3f7ee6cc1a0e7db43
takeharas24 <sayetakehara2545@gmail.com>
2023-04-25 15:00:26 -0400

    MINIGAME 2 GESTURES WORK NOW!!!!!

--- app/src/main/java/com/example/marinefinalproj/MinigameTwoFragment.kt
+++ app/src/main/java/com/example/marinefinalproj/MinigameTwoFragment.kt
@@ -3,11 +3,6 @@ package com.example.marinefinalproj
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.GestureDetectorCompat
-import androidx.core.view.MotionEventCompat
 import androidx.fragment.app.Fragment
 import com.example.marinefinalproj.databinding.FragmentMinigameTwoBinding
 
@@ -15,18 +10,22 @@ import com.example.marinefinalproj.databinding.FragmentMinigameTwoBinding
 class MinigameTwoFragment : Fragment() {
     private var _binding : FragmentMinigameTwoBinding? = null
     private val binding get() = _binding!!
-    private lateinit var mDetector: GestureDetector
+    private val gesture = GestureDetector(
+        activity,
+        object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent): Boolean {
+                return true
+            }
 
-    private var gestureListener: GestureDetector.SimpleOnGestureListener = object: GestureDetector.SimpleOnGestureListener(){
-        override fun onFling(
-            e1: MotionEvent,
-            e2: MotionEvent,
-            velocityX: Float,
-            velocityY: Float
-        ): Boolean {
-            return super.onFling(e1, e2, velocityX, velocityY)
-        }
-    }
+            override fun onFling(
+                e1: MotionEvent, e2: MotionEvent, velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                Log.i("MainActivity", "onFling has been called!")
+                return super.onFling(e1, e2, velocityX, velocityY)
+            }
+        })
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,8 +33,11 @@ class MinigameTwoFragment : Fragment() {
         _binding = FragmentMinigameTwoBinding.inflate(inflater, container, false)
         val rootView = binding.root
         //Minigame two: fling something? Maybe sand
-        GestureDetector.SimpleOnGestureListener()
-        mDetector = GestureDetector(this.requireContext(), gestureListener)
+        rootView.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                return gesture.onTouchEvent(event)
+            }
+        })
         return rootView
     }
 }
